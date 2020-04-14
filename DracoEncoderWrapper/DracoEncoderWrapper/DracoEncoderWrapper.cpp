f7a2e7caf87f7bd2a4af6d6dba6fc50216d2a8e2
Daniel Webr <dwebr@gapps.zcu.cz>
2020-04-14 12:56:12 +0200

    wrapper

---
+++ DracoEncoderWrapper/DracoEncoderWrapper/DracoEncoderWrapper.cpp
@@ -0,0 +1,10 @@
+﻿// DracoEncoderWrapper.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
+//
+
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "Hello World!\n";
+}
