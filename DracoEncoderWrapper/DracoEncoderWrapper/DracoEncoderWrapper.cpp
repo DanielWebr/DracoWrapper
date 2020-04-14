bc9102aa222c7d7d0d7879a83308aaf2b0b4723b
Daniel Webr <dwebr@gapps.zcu.cz>
2020-04-14 13:28:55 +0200

    wrapper compression arguments

--- DracoEncoderWrapper/DracoEncoderWrapper/DracoEncoderWrapper.cpp
+++ DracoEncoderWrapper/DracoEncoderWrapper/DracoEncoderWrapper.cpp
@@ -1,10 +1,16 @@
-﻿// DracoEncoderWrapper.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
-//
+﻿#include <iostream>
+#include <algorithm>
+#include <iterator>
 
-#include <iostream>
-
-int main()
+int main(int argc, char* argv[])
 {
+    // print all command line arguments
+    std::cout << "name of program: " << argv[0] << '\n';
 
-    std::cout << "Hello World!\n";
+    if (argc > 1)
+    {
+        std::cout << "there are " << argc - 1 << " (more) arguments, they are:\n";
+
+        std::copy(argv + 1, argv + argc, std::ostream_iterator<const char*>(std::cout, "\n"));
+    }
 }
